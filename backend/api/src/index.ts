997a265065688b31e2d72bcd2c0419d92963a624
Jay <jordanbell321@gmail.com>
2026-01-10 05:22:13 +0000

    push

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import { loadChallenges } from "./utils/challengeLoader";
 
 const app = express();
-const port = 3000;
+const port = 22;
 
 app.use(cors());
 app.use(express.json());
